fd1feb87463ae1828d968daa58d27e06d17873fd
CahidArda <57228345+CahidArda@users.noreply.github.com>
2020-07-02 20:56:12 +0300

    Added more methods

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,15 +1,19 @@
 package game;
+import java.util.Random;
 import java.util.TreeMap;
 import java.util.Vector;
 
-import Board.GameBoard;
-import Board.GamePiece;
+import board.GameBoard;
+import board.GamePiece;
+import board.Move;
 
 public abstract class Game {
 	public GameBoard gameBoard;
 	public int size;
+	private Random r;
 	
 	Game(int size) {
+		r = new Random(0);
 		this.size = size;
 		gameBoard = new GameBoard(size, this, setInitialPieces());
 		GamePiece.pieceMovements = setPieceMovements();	
@@ -23,13 +27,33 @@ public abstract class Game {
 	
 	//abstract int[] getScores();
 	
-	GamePiece getPieceIn(int[] indexes) {
-		return gameBoard.getPieceIn(indexes);
+	private final Vector<Move> getAllMoves() {
+		Vector<Move> allMoves = new Vector<Move>();
+		for (GamePiece gp: gameBoard.getAllPieces() ) {
+			allMoves.addAll(gp.getPossibleMovesForPiece());
+		}
+		return allMoves;
+	}
+	
+	private final Vector<Move> getAllMoves(int playerId) {
+		Vector<Move> allMoves = new Vector<Move>();
+		for (GamePiece gp: gameBoard.getAllPieces() ) {
+			if (gp.getPlayerId()==playerId)
+				allMoves.addAll(gp.getPossibleMovesForPiece());
+		}
+		return allMoves;
+	}
+
+	public final void makeRandomMove(int playerId) {
+		Vector<Move> allMoves = getAllMoves(playerId);
+		int a = r.nextInt() % allMoves.size();
+		Move m = allMoves.elementAt(a>0? a: -a);
+		gameBoard.updateWithMove(m);
 	}
 	
 	public GamePiece getPieceIn(int x, int y) {
 		return gameBoard.getPieceIn(x, y);
-	} 
+	}
 	
 	
 }
