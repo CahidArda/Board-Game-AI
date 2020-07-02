542804b37f9972d702da3c04709980d071be7ef3
CahidArda <57228345+CahidArda@users.noreply.github.com>
2020-07-02 20:56:27 +0300

    Wrote different tests

--- src/Executable/Main.java
+++ src/Executable/Main.java
@@ -1,16 +1,20 @@
-package Executable;
-import Board.GamePiece;
-import Board.Move;
+package executable;
+import board.GamePiece;
+import board.Move;
 import game.EightPawnGame;
 import game.Game;
 
 public class Main {
 	public static void main(String[] args) {
-		Game g = new EightPawnGame(8);
+		test2();
+	}
+	
+	public static void test1() {
+		Game g = new EightPawnGame(10);
 		g.gameBoard.printGameBoardToConsole();
 		
 		int x = 1;
-		for (int y=1; y<7; y++) {
+		for (int y=1; y<8; y++) {
 			GamePiece gp = g.getPieceIn(x, y);
 			int[] to = {x, y+1};
 			
@@ -22,6 +26,21 @@ public class Main {
 				System.out.println(m1);
 			}
 			g.gameBoard.updateWithMove(m);
+			System.out.println();
+			g.gameBoard.printGameBoardToConsole();
+			System.out.println(g.gameBoard.getNumberOfPiecesOnTheBoard());	
+		}
+	}
+	
+	public static void test2() {
+		Game g = new EightPawnGame(4);
+		g.gameBoard.printGameBoardToConsole();
+		for (int i=0; i<5; i++) {
+			g.makeRandomMove(0);
+			System.out.println();
+			g.gameBoard.printGameBoardToConsole();
+			System.out.println(g.gameBoard.getNumberOfPiecesOnTheBoard());
+			g.makeRandomMove(1);
 			System.out.println();
 			g.gameBoard.printGameBoardToConsole();
 			System.out.println(g.gameBoard.getNumberOfPiecesOnTheBoard());
