4ae5608f98283c9289eaf9771b29d1d5104b645f
CahidArda <57228345+CahidArda@users.noreply.github.com>
2020-07-02 18:48:33 +0300

    Implemented comparable and switched folders

--- src/Board/Move.java
+++ src/Board/Move.java
@@ -1,11 +1,13 @@
+package Board;
 
-public class Move {
-	private static int lastId;
-	int moveId;
+public class Move implements Comparable<Move> {
 	GamePiece pieceToMove;
 	int[] to;
 	
-	Move(GamePiece pieceToMove, int[] to) {
+	private static int lastId = 0;
+	private int moveId;
+	
+	public Move(GamePiece pieceToMove, int[] to) {
 		moveId = lastId++;
 		this.pieceToMove = pieceToMove;
 		this.to = to;
@@ -15,16 +17,20 @@ public class Move {
 		return String.format("Moving %s, from (%d, %d) to (%d, %d)", pieceToMove.tags[pieceToMove.tagId], pieceToMove.positionIndexes[0], pieceToMove.positionIndexes[1], to[0], to[1]);
 	}
 	
-	public int hashCode() {
-		return moveId;
-	}
-	
 	public boolean equals(Object other){
 		if (other instanceof Move) {
 			Move m = (Move) other;
 			return (m.pieceToMove.pieceId == pieceToMove.pieceId && m.to[0] == to[0] && m.to[1] == to[1]);
 		}
 		return false;
+	}
+
+	@Override
+	public int compareTo(Move other) {
+		return (pieceToMove.positionIndexes[0]-other.pieceToMove.positionIndexes[0])*1000+
+				(pieceToMove.positionIndexes[1]-other.pieceToMove.positionIndexes[1])*100+
+				(to[0]-other.to[0])*10+
+				(to[1]-other.to[1]);
 	}
 	
 	
